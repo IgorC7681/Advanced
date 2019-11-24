3758ff7b8c623526458e30d3fec34bfcf45648d0
TaiYIChien <boss760801@gmail.com>
2019-11-24 16:38:50 +0800

    Initial Commit

---
+++ Advanced/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Advanced
+//
+//  Created by Tai on 2019/11/24.
+//  Copyright © 2019 Tai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
